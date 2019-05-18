69f381b9686df2414ddbbfbcb4963568ac6a285a
shreelvi <shreelvi@isu.edu>
2019-05-18 11:49:34 -0600

    conditional and coalescing operators, auto implemented and read only property

--- Language_Features/Controllers/HomeController.cs
+++ Language_Features/Controllers/HomeController.cs
@@ -16,11 +16,16 @@ namespace Language_Features.Controllers
             List<string> results = new List<string>();
             foreach (Product p in Product.GetProducts())
             {
-                string name = p?.Name;
-                decimal? price = p?.Price;
+                // Combining the Conditional and Coalescing Operators.
+                // The null conditional operator prevents a NullReferenceException when navigating
+                // through the object properties, and the null coalescing operator 
+                // ensures that I don’t include null values in
+                // the results displayed in the browser
 
-                //Chaining the null conditional operator
-                string relatedName = p?.Related?.Name;
+                string name = p?.Name ?? "<No Name>";
+                decimal? price = p?.Price ?? 0;
+                string relatedName = p?.Related?.Name ?? "<None>";
+
                 results.Add(string.Format("Name: {0}, Price: {1}, Related: {2}",
                 name, price, relatedName));
 
--- Language_Features/Models/Product.cs
+++ Language_Features/Models/Product.cs
@@ -8,13 +8,17 @@ namespace Language_Features.Models
     public class Product
     {
         public string Name { get; set; }
+        public string Category { get; set; } = "Watersports"; //Using auto-implemented property initializer
         public decimal? Price { get; set; }
-        public Product Related { get; set; }
+        public Product Related { get; set; }
+        public bool InStock { get; } = true; // Creating read-Only automatically implemented properties
+
         public static Product[] GetProducts()
         {
             Product kayak = new Product
             {
                 Name = "Kayak",
+                Category = "Water Craft",
                 Price = 275M
             };
             Product lifejacket = new Product
