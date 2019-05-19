40b68fa77493ca45630c0f3e9605018a7690ddae
shreelvi <shreelvi@isu.edu>
2019-05-19 11:56:11 -0600

    async and await keywords

--- Language_Features/Controllers/HomeController.cs
+++ Language_Features/Controllers/HomeController.cs
@@ -11,19 +11,14 @@ namespace Language_Features.Controllers
     public class HomeController : Controller
     {
 
-        // Expressing an Action Method as a Lambda Expression 
-
-        // Using Type Inference and Anonymous Types 
-        public IActionResult Index()
+        //Defining an Asynchronous Action Methods in the HomeController
+        public async Task<ViewResult> Index()
         {
-            var products = new[] { new { Name = "Kayak", Price = 275M },
-                           new { Name = "Lifejacket", Price = 48.95M },
-                           new { Name = "Soccer ball", Price = 19.50M },
-                           new { Name = "Corner flag", Price = 34.95M }
-            };
+            //var products = new[] { new { Name = "Kayak", Price = 275M },
+            long? length = await MyAsyncMethods.GetPageLength();
+            return View(new string[] { $"Length: {length}" });
 
-            return View(products.Select(p => p.Name));
-            //return View(products.Select(p => p.GetType().Name)); //All the objects in product array will have same type                                                                 //Because they have same properties
+
         }
 
     }
--- Language_Features/Models/MyAsyncMethods.cs
+++ Language_Features/Models/MyAsyncMethods.cs
@@ -13,17 +13,15 @@ namespace Language_Features.Models
     {
         // This method uses a System.Net.Http.HttpClient object to request the 
         // contents of the Apress homepage and returns its length
-        public static Task<long?> GetPageLength()
+        // Approach in last method is long and confusing.. 
+        // So we use async and await keyword
+        public async static Task<long?> GetPageLength()
         {
             HttpClient client = new HttpClient();
-            var httpTask = client.GetAsync("http://apress.com"); // get back is a Task<HttpResponseMessage> .
 
-            // we could do other things here while the HTTP request is performed
-            // continue with specifies we are returning a Task<HttpResponseMessage > object
-            return httpTask.ContinueWith((Task<HttpResponseMessage> antecedent) =>
-            {
-                return antecedent.Result.Content.Headers.ContentLength;//  return the length of the ContentLength header. 
-            });
+            var httpMessage = await client.GetAsync("http://apress.com");
+
+            return httpMessage.Content.Headers.ContentLength;
         }
     }
 }
